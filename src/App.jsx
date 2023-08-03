b72afcc5c606333feb8395122a0648c74146c527
Travis Alstrand <travisalstrand@gmail.com>
2023-08-03 15:43:19 -0700

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import Testimonials from "./components/Testimonials";
+
+export default function App() {
+  return (
+    <>
+      <Hero />
+      <Services />
+      <About />
+      <Contact />
+      <Testimonials />
+    </>
+  );
+}
